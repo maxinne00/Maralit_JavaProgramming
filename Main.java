8d0fd7fefd5253288c0002aff14c455a4e6c1343
maxinne00 <maxinneamaralit@gmail.com>
2026-07-07 16:58:30 +0800

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,35 @@
+public class Main {
+
+    // this is my first performance task in java programming
+    // I hope you enjoyed reading this code 
+    // and I hope you learned something from it
+    /*this
+    is a
+    multiline comment
+    */
+
+    public static void main(String[] args) {
+        System.out.println(" ___________________________________");
+        System.out.println("|                                   |");
+        System.out.println("|  JOSE ABAD SANTOS MEMORIAL SCHOOL |" );
+        System.out.println("|___________________________________|");
+        System.out.println("                                    ");
+        System.out.println("Welcome to PWU-JASMS QC, we are glad to have you here!");
+        System.out.print("As an ICT Student..");
+        System.out.println("all we can say is DO IT SCARED!");
+        System.out.println("============================================================================");
+        System.out.print("Teacher:");
+        System.out.println("Mr. Lan Emmanuel Sevillino");
+        System.out.println("You don't have to be great to start, but you have to start to be great.:");
+
+        System.out.println("============================================================================");
+        System.out.println("PERSONAL INFORMATION");
+        System.out.print("Name:");
+        System.out.println("Maxinne A. Maralit");
+        System.out.print("Grade and Strand:");
+        System.out.println("12 - ICT");
+    
+} 
+}
+
+
