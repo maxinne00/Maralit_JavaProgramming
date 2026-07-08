b4298eda66ec9050b9ca920cb51f84b853b52631
maxinne00 <maxinneamaralit@gmail.com>
2026-07-08 16:22:51 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -9,27 +9,49 @@ public class Main {
     */
 
     public static void main(String[] args) {
-        System.out.println(" ___________________________________");
-        System.out.println("|                                   |");
-        System.out.println("|  JOSE ABAD SANTOS MEMORIAL SCHOOL |" );
-        System.out.println("|___________________________________|");
+        System.out.println(".+\"+.+\"+.+\"+.+\"+.+\"+.+\"+.+\"+.+\"+.+\"+.+\"+.+\"+.+\"+.+\"+.+\"+.+\"+.+\"+.\"+.+\"+. ");
+        System.out.println(    "");       
+        System.out.println("        .:'':..:'':..:'':..:'':..:'':..:'':.");
+        System.out.println("        |                                   |");
+        System.out.println("        |  JOSE ABAD SANTOS MEMORIAL SCHOOL |" );
+        System.out.println("        |                                   |");
+        System.out.println("        ':..:'':..:'':..:'':..:'':..:'':..:''");
         System.out.println("                                    ");
         System.out.println("Welcome to PWU-JASMS QC, we are glad to have you here!");
-        System.out.print("As an ICT Student..");
+        System.out.print("As an ICT Student....");
         System.out.println("all we can say is DO IT SCARED!");
-        System.out.println("============================================================================");
+        System.out.println("_.-=-._.-=-._.-=-._.-=-._.-=-._.-=-._.-=-._.-=-._.-=-._.-=-._.-=-._");
+        System.out.println(    "");
         System.out.print("Teacher:");
-        System.out.println("Mr. Lan Emmanuel Sevillino");
-        System.out.println("You don't have to be great to start, but you have to start to be great.:");
-
-        System.out.println("============================================================================");
-        System.out.println("PERSONAL INFORMATION");
+        System.out.println(" Mr. Lan Emmanuel Sevillino");
+        System.out.println("You don't have to be great to start, but you have to start to be great.");
+        System.out.println(    "");
+        System.out.println("_.-=-._.-=-._.-=-._.-=-._.-=-._.-=-._.-=-._.-=-._.-=-._.-=-._.-=-._");
+        System.out.println("                                    "); 
+        System.out.println("=================  PERSONAL INFORMATION  ===================");
+        System.out.println("                                    ");
         System.out.print("Name:");
-        System.out.println("Maxinne A. Maralit");
+        System.out.println(" Maxinne A. Maralit");
         System.out.print("Grade and Strand:");
-        System.out.println("12 - ICT");
-    
-} 
+        System.out.println(" 12 - ICT");
+        System.out.print("What is ICT for you?");
+        System.out.println(    "");
+        System.out.println(" -For me ICT is a way to express my creativity & to learn new things about technology.");
+        System.out.print("Why did you choose ICT?");
+        System.out.println(    "");
+        System.out.println(" -For me it's a way to combine my love for technology and creativity.");
+        System.out.println("My future Job?:");
+        System.out.println(" -In the future, I want to be a doctor because I want to help people.");
+        System.out.println(    "");
+        System.out.println("Do the best you can until you know better. Then when you know better, do better. ");
+        System.out.println(    "");
+        System.out.println("     .-\"-.              .-\"-.              .-\"-.             .-\"-.");
+        System.out.println("    /|6 6|\\            /|6 6|\\            /|6 6|\\           /|6 6|\\");           
+        System.out.println("   {/(_0_)\\}          {/(_0_)\\}          {/(_0_)\\}         {/(_0_)\\}"); 
+        System.out.println(    "");
+        System.out.println(".+\"+.+\"+.+\"+.+\"+.+\"+.+\"+.+\"+.+\"+.+\"+.+\"+.+\"+.+\"+.+\"+.+\"+.+\"+.+\"+.\"+.+\"+. ");
+
+    }
 }
 
 
