c8f84052fe72e26b6ac984c5a19d9627998f574a
maxinne00 <maxinneamaralit@gmail.com>
2026-07-10 17:50:58 +0800

    Create Activity1.java

---
+++ Activity1.java
@@ -0,0 +1,30 @@
+public class studentInformation{
+    // this is my activity 1
+
+    public static void main (String [] args) {
+
+    String studentName= "Maxinne";
+    int age= 17;
+    String gradeLevel="Grade 12";
+    String school="Jose Abad Santos Memorial School";
+    String favoriteSubject="Philosophy";
+    Double generalAverage="93"
+    Boolean honorStudent=true;
+
+    System.Out.println("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-");
+    System.Out.println("                                                             ");
+    System.Out>println(".-=-._.-=-._.-=-._.-=STUDENT INFORMATION=-._.-=-._.-=-._.-=-.");
+    System.Out.println("Student Name:" + studentName);
+    System.Out>println("Student Age:" + age )
+    System.Out.println("Grade Level:" + gradeLevel);
+    System.Out.println("School:" + school);
+    System.Out.println("Favorite Subject:" + favoriteSubject);
+    System.Out.println("Favorite Subject:" + favoriteSubject);
+    System.Out.println("                                                             ");
+    System.Out.println("Average" + generalAverage);
+    System.Out.println("Honor Student:"+ honorStudent)
+    System.Out.println("                                                             ");
+    System.Out.println("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-");
+    }
+
+}
