7b0b4e5dc18b7a2af1dfe472b0658357b80656ca
maxinne00 <maxinneamaralit@gmail.com>
2026-07-21 16:59:59 +0800

    daming red

--- Activity7.java
+++ Activity7.java
@@ -1,1 +1,25 @@
 
+
+import java.util.Scanner;
+
+public class Activity7 {
+    public static void main(String[] args) {
+        Scanner email  = new Scanner (System.in);
+        System.out.println("ENTER YOUR EMAIL:");
+        String userEmail = email.nextLine();
+
+        String user = "locate the @";
+        System.out.println( user.indexOf("@"));
+
+        int atIndex = email.indexOf("@");
+
+        String username =  email.substring( 0,  atIndex);
+        String domain =  email.substring( atIndex + 1);
+
+        System.out.println("Enter Username");
+
+      
+
+
+    } 
+}
